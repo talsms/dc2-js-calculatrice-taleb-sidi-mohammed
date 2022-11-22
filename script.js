6899426b4d0239169891ff267d9d385f8b56fe79
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2022-11-22 11:27:04 +0100

    Ajout du code pour les fonctions

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log( "Hey it's me i'm the problem");
+var number1;
+var number2;
+var result = 0; 
+
+function sum(number1, number2) {
+  result = number1 + number2;
+}
+
+function mult(number1, number2) {
+	result = number1 * number2;
+	}
+
+function div(number1, number2) {
+	result = number1 / number2;
+	
+}
+
+function sub(number1, number2) {
+	result = number1 - number2;
+}
+
+
+console.log(result);
